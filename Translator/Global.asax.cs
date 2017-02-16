952f9625b7462dcfa89184940d1734c5d6c8f2b8
Jonathan Pickett <jonathan_pickett@outlook.com>
2017-02-15 19:11:05 -0800

    namespace fix

--- Translator/Global.asax.cs
+++ Translator/Global.asax.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
 
-namespace Translator
+namespace TranslatorBot
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
